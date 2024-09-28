309d1e894dead062e945655575006d2d4a9ba0f1
Ilya Gulya <ilya.gulya@indriver.com>
2024-09-28 08:26:24 +0500

    Fixes

--- Sources/XcodeProj/Extensions/Path+Extras.swift
+++ Sources/XcodeProj/Extensions/Path+Extras.swift
@@ -5,18 +5,38 @@ import PathKit
 
 // MARK: - Path extras.
 
+#if os(macOS)
+import Darwin
+#elseif os(Windows)
+import ucrt
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(Bionic)
+import Bionic
+#elseif canImport(Musl)
+import Musl
+#else
+import CoreFoundation
+#endif
+
 func systemGlob(_ pattern: UnsafePointer<CChar>!, _ flags: Int32, _ errfunc: (@convention(c) (UnsafePointer<CChar>?, Int32) -> Int32)!, _ vector_ptr: UnsafeMutablePointer<glob_t>!) -> Int32 {
     #if os(macOS)
-        return Darwin.glob(pattern, flags, errfunc, vector_ptr)
-    #else
-        return Glibc.glob(pattern, flags, errfunc, vector_ptr)
+    return Darwin.glob(pattern, flags, errfunc, vector_ptr)
+    #elseif os(Windows)
+    return ucrt.glob(pattern, flags, errfunc, vector_ptr)
+    #elseif canImport(Glibc)
+    return Glibc.glob(pattern, flags, errfunc, vector_ptr)
+    #elseif canImport(Bionic)
+    return Bionic.glob(pattern, flags, errfunc, vector_ptr)
+    #elseif canImport(Musl)
+    return Musl.glob(pattern, flags, errfunc, vector_ptr)
     #endif
 }
 
 extension Path {
     /// Creates a directory
     ///
-    /// - Throws: an errof if the directory cannot be created.
+    /// - Throws: an error if the directory cannot be created.
     func mkpath(withIntermediateDirectories: Bool) throws {
         try FileManager.default.createDirectory(atPath: string, withIntermediateDirectories: withIntermediateDirectories, attributes: nil)
     }
@@ -35,7 +55,15 @@ extension Path {
             free(cPattern)
         }
 
-        let flags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
+        #if os(Linux)
+          #if canImport(Musl)
+            let flags = GLOB_TILDE | GLOB_MARK
+          #else
+            let flags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
+          #endif
+        #else
+          let flags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
+        #endif
         if systemGlob(cPattern, flags, nil, &gt) == 0 {
             #if os(macOS)
                 let matchc = gt.gl_matchc
