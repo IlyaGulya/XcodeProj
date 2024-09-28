584f725d5dc78995ecdfb76ab699168a8b3b2f1f
Ilya Gulya <ilya@gulya.me>
2024-09-28 09:39:49 +0500

    Fix

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.6.1")),
-        .package(url: "https://github.com/IlyaGulya/PathKit-musl", branch: "master"),
+        .package(url: "https://github.com/IlyaGulya/PathKit", branch: "master"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
     ],
     targets: [
         .target(name: "XcodeProj",
                 dependencies: [
-                    .product(name: "PathKit", package: "PathKit-musl"),
-                    .product(name: "AEXML", package: "AEXML"),
+                  "PathKit",
+                  .product(name: "AEXML", package: "AEXML"),
                 ],
                 swiftSettings: [
                     .enableExperimentalFeature("StrictConcurrency"),
